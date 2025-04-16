04aced9f14c4d27686d7f1bc75c56624e6b17616
Miguel-Lessa <meu1emeio1soh1meu@gmail.com>
2025-04-16 19:38:24 -0300

    eh

---
+++ backend/ttos/src/main/java/com/ttos/ttos/shared/controller/RegisterController.java
@@ -0,0 +1,15 @@
+package com.ttos.ttos.shared.controller;
+
+import com.ttos.ttos.model.User;
+import com.ttos.ttos.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+
+@Controller
+
+public class RegisterController {
+    
+}
